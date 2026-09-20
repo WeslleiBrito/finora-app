4fb04a2bfe2edf2532ca2b6f5937506471f8b504
WeslleiBrito <wesllei.wbs@gmail.com>
2026-09-20 15:22:57 -0300

    removendo chave

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
     server: {
       host: "0.0.0.0",
       allowedHosts: [
-        "mailed-auckland-genius-roland.trycloudflare.com",
-        "much-plane-williams-paris.trycloudflare.com",
-        "breathing-laundry-facts-copyrighted.trycloudflare.com",
         "nucleus-onshore-ivory.ngrok-free.dev"
       ],
       proxy: {
